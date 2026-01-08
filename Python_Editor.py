ef5c6598e1b77586b24e85d4af01655212bb015b
Stacey Syphus <22669486+stever-sas@users.noreply.github.com>
2026-01-08 11:02:04 -0800

    Add copyright date

--- Python_Editor.py
+++ Python_Editor.py
@@ -1,3 +1,7 @@
+## Copyright © 2025, SAS Institute Inc., Cary, NC, USA.  All Rights Reserved.
+##  SPDX-License-Identifier: Apache-2.0      
+
+
 ## Packages and Options
 import pandas as pd
 import numpy as np
